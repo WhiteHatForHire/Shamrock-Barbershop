2656952d9ce93102fa8a13a36d6a9a021a0dad60
whitehatforhire <stephensm2011@gmail.com>
2018-05-06 18:07:28 -0400

    added app folder, set up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 var reload = browserSync.reload;
 
-// watch files for changes and reload
-// gulp.task('serve', function() {
-//   browserSync({
-//     server: {
-//       baseDir: 'app'
-//     }
-//   });
 
-//   gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
-// });
+// compile sass
+gulp.task('sass', function () {
+  return gulp.src('./dev/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./app/css'));
+});
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
